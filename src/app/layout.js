7f65b78d2697d9a6406af10ef16c62a73a54095f
BAGUIAN01 <baguian.harouna7231@gmail.com>
2025-07-19 14:48:52 +0200

    feat: add phototeque

--- src/app/layout.js
+++ src/app/layout.js
@@ -106,10 +106,9 @@ export const metadata = {
   // Métadonnées supplémentaires pour le logo
   icons: {
     icon: [
-      { url: '/favicon.ico', sizes: '48x48' },
-      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
-      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
-      { url: '/favicon-96x96.png', sizes: '96x96', type: 'image/png' },
+      { url: '/logo_short-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/logo_short-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/logo_short-96x96.png', sizes: '96x96', type: 'image/png' },
     ],
     apple: [
       { url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' },
@@ -175,15 +174,14 @@ export default function RootLayout({
   return (
     <html lang="fr" className={`${inter.variable} ${poppins.variable}`}>
       <head>
-        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/logo_short-16x16.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/logo_short-32x32.png" />
+        <link rel="icon" type="image/png" sizes="96x96" href="/logo_short-96x96.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/logo_short.png" />
         <link rel="manifest" href="/site.webmanifest" />
         <meta name="theme-color" content="#010066" />
         <meta name="msapplication-TileColor" content="#010066" />
-        <meta name="msapplication-TileImage" content="/favicon-144x144.png" />
+        <meta name="msapplication-TileImage" content="/logo_short-144x144.png" />
       </head>
       <body className="min-h-screen bg-background font-sans antialiased">
         <div className="min-h-screen flex flex-col">
